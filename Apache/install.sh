ff00cad400d191f3e4f896d0a040c01d64999c62
Jacob <78283092+Dunn-Dev8@users.noreply.github.com>
2022-12-04 08:48:29 +1030

    Add files via upload

---
+++ Apache/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+echo "Make sure Apache2 is installed Before Running"
+echo "sudo apt update"
+echo "sudo apt install apache2"
+sleep .5
+
+sudo cp ../index.html /var/www/html
+sudo cp ../Contact.html /var/www/html
+sudo cp -R ../posts/ /var/www/html
+sudo cp ../favicon.ico /var/www/html
